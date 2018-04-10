a05702065e6ba6663ac9e474cc801d87b18402dd
Oli Bullen <d33con@gmail.com>
2018-04-10 21:58:16 +0100

    add initializeBoard function

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,19 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = props => {
   return (
     <header>
       <div>Memory Game</div>
-      <div>New Game</div>
+      <div className="btn" onClick={props.initializeBoard}>
+        New Game
+      </div>
     </header>
   );
+};
+
+Header.propTypes = {
+  initializeBoard: PropTypes.func.isRequired
 };
 
 export default Header;
