c1cf8c67b3da63fcea836cc76e70994f1f33c02a
Oli Bullen <d33con@gmail.com>
2018-04-11 16:56:57 +0100

    move state up to App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,92 @@ import GameBoard from "./GameBoard";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      board: [
+        { id: 0, status: "notShowing", backgroundColor: "blue" },
+        { id: 1, status: "notShowing", backgroundColor: "blue" },
+        { id: 2, status: "notShowing", backgroundColor: "orange" },
+        { id: 3, status: "notShowing", backgroundColor: "orange" },
+        { id: 4, status: "notShowing", backgroundColor: "yellow" },
+        { id: 5, status: "notShowing", backgroundColor: "yellow" },
+        { id: 6, status: "notShowing", backgroundColor: "red" },
+        { id: 7, status: "notShowing", backgroundColor: "red" },
+        { id: 8, status: "notShowing", backgroundColor: "green" },
+        { id: 9, status: "notShowing", backgroundColor: "green" },
+        { id: 10, status: "notShowing", backgroundColor: "purple" },
+        { id: 11, status: "notShowing", backgroundColor: "purple" },
+        { id: 12, status: "notShowing", backgroundColor: "brown" },
+        { id: 13, status: "notShowing", backgroundColor: "brown" },
+        { id: 14, status: "notShowing", backgroundColor: "cyan" },
+        { id: 15, status: "notShowing", backgroundColor: "cyan" }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    this.shuffleCards(this.state.board);
+  }
+
+  initializeBoard = () => {
+    this.state.board.map(tile => (tile.status = "notShowing"));
+    this.shuffleCards(this.state.board);
+  };
+
+  shuffleCards = board => {
+    for (let i = board.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [board[i], board[j]] = [board[j], board[i]];
+    }
+    this.setState({ board });
+  };
+
+  revealTile = (id, color) => {
+    const board = this.state.board.map(square => {
+      if (square.id === id && square.status === "notShowing") {
+        square.status = "isShowing";
+      }
+      return square;
+    });
+    this.setState({ board });
+    const clickedTiles = board.filter(tile => tile.status === "isShowing");
+    clickedTiles.length === 2 && this.checkForMatch(this.state.board, color);
+  };
+
+  checkForMatch = (board, color) => {
+    const colors = board.filter(
+      tile => tile.backgroundColor === color && tile.status !== "notShowing"
+    );
+    if (colors.length > 1) {
+      const matchingTiles = this.state.board.filter(
+        tile =>
+          tile.backgroundColor === color && tile.status === "isShowing"
+            ? (tile.status = "matched")
+            : tile
+      );
+      this.setState({ board: matchingTiles });
+    }
+
+    setTimeout(() => {
+      const showingTiles = board.filter(
+        tile =>
+          tile.status === "isShowing" ? (tile.status = "notShowing") : tile
+      );
+      this.setState({ board: showingTiles });
+    }, 1000);
+  };
+
   render() {
     return (
       <div className="App">
-        <Header />
-        <GameBoard />
+        <Header initializeBoard={this.initializeBoard} />
+        <GameBoard
+          board={this.state.board}
+          revealTile={this.revealTile}
+          shuffleCards={this.shuffleCards}
+        />
       </div>
     );
   }
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -2,87 +2,18 @@ import React, { Component } from "react";
 import Tile from "./Tile";
 
 class GameBoard extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      board: [
-        { id: 0, status: "notShowing", backgroundColor: "blue" },
-        { id: 1, status: "notShowing", backgroundColor: "blue" },
-        { id: 2, status: "notShowing", backgroundColor: "orange" },
-        { id: 3, status: "notShowing", backgroundColor: "orange" },
-        { id: 4, status: "notShowing", backgroundColor: "yellow" },
-        { id: 5, status: "notShowing", backgroundColor: "yellow" },
-        { id: 6, status: "notShowing", backgroundColor: "red" },
-        { id: 7, status: "notShowing", backgroundColor: "red" },
-        { id: 8, status: "notShowing", backgroundColor: "green" },
-        { id: 9, status: "notShowing", backgroundColor: "green" },
-        { id: 10, status: "notShowing", backgroundColor: "purple" },
-        { id: 11, status: "notShowing", backgroundColor: "purple" },
-        { id: 12, status: "notShowing", backgroundColor: "brown" },
-        { id: 13, status: "notShowing", backgroundColor: "brown" },
-        { id: 14, status: "notShowing", backgroundColor: "cyan" },
-        { id: 15, status: "notShowing", backgroundColor: "cyan" }
-      ]
-    };
-  }
-
   componentDidMount() {
-    this.shuffleCards(this.state.board);
+    this.props.shuffleCards(this.props.board);
   }
-
-  shuffleCards = board => {
-    for (let i = board.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      [board[i], board[j]] = [board[j], board[i]];
-    }
-    this.setState({ board });
-  };
-
-  revealTile = (id, color) => {
-    const board = this.state.board.map(square => {
-      if (square.id === id && square.status === "notShowing") {
-        square.status = "isShowing";
-      }
-      return square;
-    });
-    this.setState({ board });
-    const clickedTiles = board.filter(tile => tile.status === "isShowing");
-    clickedTiles.length === 2 && this.checkForMatch(this.state.board, color);
-  };
-
-  checkForMatch = (board, color) => {
-    const colors = board.filter(
-      tile => tile.backgroundColor === color && tile.status !== "notShowing"
-    );
-    if (colors.length > 1) {
-      const matchingTiles = this.state.board.filter(
-        tile =>
-          tile.backgroundColor === color && tile.status === "isShowing"
-            ? (tile.status = "matched")
-            : tile
-      );
-      this.setState({ board: matchingTiles });
-    }
-
-    setTimeout(() => {
-      // set isShowing's to notShowing
-      const showingTiles = board.filter(
-        tile =>
-          tile.status === "isShowing" ? (tile.status = "notShowing") : tile
-      );
-      this.setState({ board: showingTiles });
-    }, 1000);
-  };
 
   render() {
-    const board = this.state.board.map(square => (
+    const board = this.props.board.map(square => (
       <Tile
         id={square.id}
         key={square.id}
         backgroundColor={square.backgroundColor}
         status={square.status}
-        revealTile={this.revealTile}
+        revealTile={this.props.revealTile}
       />
     ));
     return <div className="board">{board}</div>;
