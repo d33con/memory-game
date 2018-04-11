57cf019632850c584deb19dd2751f6b433ddc6ab
Oli Bullen <d33con@gmail.com>
2018-04-11 17:12:09 +0100

    stateless functional component

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,23 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import Tile from "./Tile";
 
-class GameBoard extends Component {
-  componentDidMount() {
-    this.props.shuffleCards(this.props.board);
-  }
-
-  render() {
-    const board = this.props.board.map(square => (
-      <Tile
-        id={square.id}
-        key={square.id}
-        backgroundColor={square.backgroundColor}
-        status={square.status}
-        revealTile={this.props.revealTile}
-      />
-    ));
-    return <div className="board">{board}</div>;
-  }
+function GameBoard(props) {
+  const { board, revealTile } = props;
+  const gameBoard = board.map(square => (
+    <Tile
+      id={square.id}
+      key={square.id}
+      backgroundColor={square.backgroundColor}
+      status={square.status}
+      revealTile={revealTile}
+    />
+  ));
+  return <div className="board">{gameBoard}</div>;
 }
+
+GameBoard.propTypes = {
+  board: PropTypes.array.isRequired,
+  revealTile: PropTypes.func.isRequired
+};
 
 export default GameBoard;
